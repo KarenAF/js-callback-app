e065de84a8a53bf1aac600d63394e75e738d0859
kareningenuity <karenjxliu@hotmail.com>
2016-08-11 19:30:18 -0500

    Added color change to index.html.erb

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,2 +1,16 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+function changeColors() {
+  setTimeout(function() {
+    document.getElementById('first').style['color']= 'red';
+    setTimeout(function() {
+      document.getElementById('second').style['color']= 'blue';
+      setTimeout(function() {
+        document.getElementById('third').style['color']= 'green';
+          setTimeout(function() {
+          document.getElementById('fourth').style['color']= 'purple';
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}
